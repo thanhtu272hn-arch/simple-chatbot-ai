cb8d083dbb0415f5801acd06e24b9685233c977d
Thanh Tu Tran <thanhtu272hn@gmail.com>
2026-04-22 15:24:14 +0700

    fix static files

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
 
 from ai.core import fake_ai
 from state_manager import get_user_state, save_state
+import os
+
+print("FILES:", os.listdir("."))
 
 app = FastAPI()
 
@@ -14,12 +18,13 @@ class ChatRequest(BaseModel):
 @app.post("/chat")
 def chat(req: ChatRequest):
     state = get_user_state(req.user_id)
-
     reply = fake_ai(req.message, state)
-
-    save_state()  # 🔥 save sau mỗi request
+    save_state()
 
     return {
         "reply": reply,
         "user_id": req.user_id
     }
+
+# 🔥 PHẢI Ở CUỐI
+app.mount("/", StaticFiles(directory=".", html=True), name="static")
