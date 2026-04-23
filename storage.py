5b3ae85453aef92c2cc02ea342ad23f3d7d1c185
Thanh Tu Tran <thanhtu272hn@gmail.com>
2026-04-23 11:37:24 +0700

    fix save_all_users param

--- storage.py
+++ storage.py
@@ -1,6 +1,7 @@
 import json
 import os
 from threading import Lock
+from state_manager import USER_STATES
 
 FILE = "data.json"
 _lock = Lock()
@@ -13,7 +14,6 @@ def load_all_users():
         return json.load(f)
 
 
-def save_all_users(USER_STATES):
-    with _lock:  # 🔥 tránh race condition cơ bản
-        with open(FILE, "w", encoding="utf-8") as f:
-            json.dump(USER_STATES, f, indent=2)
+def save_all_users():
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(USER_STATES, f, indent=2)
