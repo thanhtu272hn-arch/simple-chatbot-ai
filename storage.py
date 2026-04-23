735d1ab2405e69f670913523f8e2a1f1ef66c793
Thanh Tu Tran <thanhtu272hn@gmail.com>
2026-04-23 11:03:06 +0700

    fix save_all_users bug

--- storage.py
+++ storage.py
@@ -5,13 +5,15 @@ from threading import Lock
 FILE = "data.json"
 _lock = Lock()
 
+
 def load_all_users():
     if not os.path.exists(FILE):
         return {}
     with open(FILE, "r", encoding="utf-8") as f:
         return json.load(f)
 
-def save_all_users(data):
+
+def save_all_users(USER_STATES):
     with _lock:  # 🔥 tránh race condition cơ bản
         with open(FILE, "w", encoding="utf-8") as f:
-            json.dump(data, f, indent=2)
+            json.dump(USER_STATES, f, indent=2)
